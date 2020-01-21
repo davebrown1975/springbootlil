791f37d5ee7b467764ef65b225eebe0ba129d0e9
davebrown1975 <dave@tucanoo.com>
2020-01-21 22:22:14 +0100

    Initial Commit. Boilerplate code

---
+++ src/main/java/com/tucanoo/springbootlil/SpringbootlilApplication.java
@@ -0,0 +1,13 @@
+package com.tucanoo.springbootlil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootlilApplication.class, args);
+    }
+
+}
